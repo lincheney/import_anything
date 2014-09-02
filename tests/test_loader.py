7960645750fcc6e7c4f42fbb51888366db11b4bb
Cheney Lin <lincheney@gmail.com>
2014-09-02 13:03:43 +1000

    test for Loader

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,6 +1,6 @@
 import unittest
 import unittest.mock as mock
-from import_anything import Loader
+from import_anything import Loader, Compiler
 
 class TestLoader(unittest.TestCase):
     def test_from_compiler(self):
@@ -8,8 +8,11 @@ class TestLoader(unittest.TestCase):
         .from_compiler should subclass and set COMPILER
         """
         
-        compiler = mock.sentinel.compiler
-        cls = Loader.from_compiler(compiler)
+        compiler = mock.Mock()
+        factory = Loader.from_compiler(compiler)
         
-        assert issubclass(cls, Loader)
-        self.assertIs(cls.COMPILER, compiler)
+        assert callable(factory)
+        
+        loader = factory('', [])
+        self.assertIsInstance(loader, Loader)
+        self.assertIs(loader.compiler, compiler())
