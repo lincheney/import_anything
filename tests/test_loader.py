896776565f7df78cf1b0bef0c3092237f1e23eca
Cheney Lin <lincheney@gmail.com>
2014-09-02 19:03:11 +1000

    reorganized test code

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -3,15 +3,14 @@ import unittest.mock as mock
 from unittest.mock import sentinel
 from import_anything import Loader, Compiler
 
-class TestLoader(unittest.TestCase):
-    def default_loader(self, compiler = None):
-        if compiler is None:
-            compiler = mock.Mock()
-            compiler.MAGIC = None
-            compiler.MAGIC_TAG = None
-        
-        return Loader('filename', 'path', compiler = compiler)
+def default_loader():
+    compiler = mock.Mock()
+    compiler.MAGIC = None
+    compiler.MAGIC_TAG = None
     
+    return Loader('filename', 'path', compiler = compiler)
+
+class TestLoader(unittest.TestCase):
     def test_from_compiler(self):
         """
         .from_compiler should subclass and set COMPILER
@@ -26,63 +25,6 @@ class TestLoader(unittest.TestCase):
         self.assertIsInstance(loader, Loader)
         self.assertIs(loader.compiler, sentinel.compiler)
     
-    def test_get_data_compiled(self):
-        """
-        .get_data should return the translated code
-        when re-compiling
-        """
-        
-        loader = self.default_loader()
-        result = loader.get_data('path')
-        self.assertIs(result, loader.compiler.data)
-    
-    def test_get_data_cached(self):
-        """
-        .get_data should modify the size in data
-        when retrieving cached data
-        """
-        
-        data = b'0' * 100
-        _size = 10
-        
-        with mock.patch('importlib.machinery.SourceFileLoader.get_data', return_value = data):
-            loader = self.default_loader()
-            loader._size = _size
-            loader._compiler_cls.MAGIC = None
-            
-            result = loader.get_data('different path')
-            self.assertIsInstance(result, bytes)
-            
-            magic = result[:4]
-            mtime = result[4:8]
-            size = result[8:12]
-            code = result[12:]
-            
-            self.assertEqual(magic, data[:4])
-            self.assertEqual(mtime, data[4:8])
-            self.assertEqual(size, _size.to_bytes(4, 'little'))
-            self.assertEqual(code, data[12:])
-    
-    def test_get_data_with_magic(self):
-        """
-        .get_data should remove compiler's magic from data
-        """
-        import ctypes
-        
-        compiler_magic = 10
-        data = b'0' * 100
-        
-        with mock.patch('importlib.machinery.SourceFileLoader.get_data', return_value = data):
-            loader = self.default_loader()
-            loader._size = 10
-            loader._compiler_cls.MAGIC = compiler_magic
-            
-            expected_magic = loader.apply_compiler_magic(data[:2]) + data[2:4]
-            
-            result = loader.get_data('different path')
-            
-            self.assertEqual(result[:4], expected_magic)
-    
     @mock.patch('builtins.compile')
     def test_source_to_code_compiled(self, compile):
         """
@@ -92,51 +34,11 @@ class TestLoader(unittest.TestCase):
         
         compile.return_value = sentinel.code
         
-        loader = self.default_loader()
+        loader = default_loader()
         result = loader.source_to_code(None, sentinel.path)
         
         compile.assert_called_once_with(loader.compiler.make_ast_tree(), sentinel.path, mock.ANY)
         self.assertIs(result, sentinel.code)
-    
-    @mock.patch('importlib.machinery.SourceFileLoader.set_data', return_value = sentinel.super_result)
-    def test_set_data(self, super_set_data):
-        """
-        .set_data should replace data with recompiled code
-        """
-        import marshal
-        
-        data = b'0' * 100
-        code = 'code'
-        
-        with mock.patch.object(Loader, 'source_to_code', return_value = code):
-            loader = self.default_loader()
-            result = loader.set_data('bytecode path', data)
-            
-            super_set_data.assert_called_once_with('bytecode path', data[:12] + marshal.dumps(code))
-            # returns the super call
-            self.assertIs(result, sentinel.super_result)
-    
-    @mock.patch('importlib.machinery.SourceFileLoader.set_data', return_value = sentinel.super_result)
-    def test_set_data_with_magic(self, super_set_data):
-        """
-        .set_data should apply the compiler magic to the data
-        """
-        import ctypes
-        
-        compiler_magic = 100
-        data = b'0' * 100
-        
-        with mock.patch.object(Loader, 'source_to_code', return_value = 'code'):
-            loader = self.default_loader()
-            loader._compiler_cls.MAGIC = compiler_magic
-            
-            expected_magic = loader.apply_compiler_magic(data[:2])
-            
-            result = loader.set_data('bytecode path', data)
-            
-            args = super_set_data.call_args[0]
-            self.assertEqual(args[0], 'bytecode path')
-            self.assertEqual(args[1][:2], expected_magic)
     
     @mock.patch('importlib.machinery.SourceFileLoader.path_stats')
     def test_path_stats(self, super_path_stats):
@@ -146,24 +48,133 @@ class TestLoader(unittest.TestCase):
         
         super_path_stats.return_value = dict(size = sentinel.size)
         
-        loader = self.default_loader()
+        loader = default_loader()
         result = loader.path_stats('some path')
         
         super_path_stats.assert_called_once_with('some path')
         self.assertEqual(loader._size, sentinel.size)
+
+class TestLoaderGetData(unittest.TestCase):
+    """
+    Tests for Loader.get_data
+    """
+    
+    data = b'0' * 100
+    size = 10
+    
+    def setUp(self):
+        self.get_data_patch = mock.patch('importlib.machinery.SourceFileLoader.get_data', return_value = self.data)
+        self.super_get_data = self.get_data_patch.start()
+        
+        self.loader = default_loader()
+        self.loader._size = self.size
+    
+    def tearDown(self):
+        self.get_data_patch.stop()
+    
+    
+    def test_compiled(self):
+        """
+        .get_data should return the translated code when re-compiling
+        """
+        
+        result = self.loader.get_data('path')
+        self.assertIs(result, self.loader.compiler.data)
+    
+    def test_without_magic(self):
+        """
+        .get_data should only modify the size without magic
+        """
+        
+        result = self.loader.get_data('different path')
+        
+        self.assertIsInstance(result, bytes)
+        self.assertEqual(result[:8], self.data[:8])
+        self.assertEqual(result[8:12], self.size.to_bytes(4, 'little'))
+        self.assertEqual(result[12:], self.data[12:])
+        
+    def test_with_magic(self):
+        """
+        .get_data should apply the compiler magic
+        """
+        
+        self.loader._compiler_cls.MAGIC = 10
+        expected_magic = self.loader.apply_compiler_magic(self.data[:2]) + self.data[2:4]
+        
+        result = self.loader.get_data('different path')
+        self.assertEqual(result[:4], expected_magic)
+    
+    def test_with_magic_tag(self):
+        """
+        .get_data should apply the magic tag to the
+        path that data gets loaded from
+        """
+        
+        magic_tag = 'magic-tag'
+        path = '/abc/def.py'
+        magic_path = '/abc/def.{}.py'.format(magic_tag)
+        
+        self.loader._compiler_cls.MAGIC_TAG = magic_tag
+        result = self.loader.get_data(path)
+        self.super_get_data.assert_called_once_with(magic_path)
+
+
+class TestLoadSetData(unittest.TestCase):
+    data = b'0' * 100
+    code = 'code'
+    
+    def setUp(self):
+        self.set_data_patch = mock.patch('importlib.machinery.SourceFileLoader.set_data', return_value = sentinel.super_result)
+        self.super_set_data = self.set_data_patch.start()
+        
+        self.source_to_code_patch = mock.patch.object(Loader, 'source_to_code', return_value = self.code)
+        self.source_to_code_patch.start()
+        
+        self.loader = default_loader()
+    
+    def tearDown(self):
+        self.set_data_patch.stop()
+        self.source_to_code_patch.stop()
+    
+    
+    def test_set_data_no_magic(self):
+        """
+        .set_data should replace data with recompiled code
+        """
+        import marshal
+        
+        result = self.loader.set_data('bytecode path', self.data)
+        self.super_set_data.assert_called_once_with('bytecode path', self.data[:12] + marshal.dumps(self.code))
+        self.assertIs(result, sentinel.super_result)
+    
+    def test_set_data_with_magic(self):
+        """
+        .set_data should apply the compiler magic
+        """
+        
+        compiler_magic = 100
+        self.loader._compiler_cls.MAGIC = compiler_magic
+        
+        result = self.loader.set_data('bytecode path', self.data)
+        args = self.super_set_data.call_args[0]
+        self.assertEqual(args[0], 'bytecode path')
+        self.assertEqual(args[1][:2], self.loader.apply_compiler_magic(self.data[:2]))
+
+
+class TestLoaderApplyCompilerMagic(unittest.TestCase):
+    data = b'0' * 100
+    
+    def setUp(self):
+        self.loader = default_loader()
+    
     
     def test_apply_compiler_magic_no_magic(self):
         """
         .apply_compiler_magic should do nothing with no magic
         """
         
-        data = b'0' * 100
-        
-        loader = self.default_loader()
-        loader._compiler_cls.MAGIC = None
-        
-        result = loader.apply_compiler_magic(data)
-        self.assertEqual(result, data)
+        result = self.loader.apply_compiler_magic(self.data)
+        self.assertEqual(result, self.data)
     
     def test_apply_compiler_magic_with_magic(self):
         """
@@ -172,30 +183,35 @@ class TestLoader(unittest.TestCase):
         import ctypes
         
         compiler_magic = 10
-        data = b'0' * 100
         
-        magic_data = int.from_bytes(data[:2], 'little') ^ ~compiler_magic
+        magic_data = int.from_bytes(self.data[:2], 'little') ^ ~compiler_magic
         magic_data = ctypes.c_uint16(magic_data).value.to_bytes(2, 'little')
-        magic_data = magic_data + data[2:]
+        magic_data = magic_data + self.data[2:]
         
-        loader = self.default_loader()
-        loader._compiler_cls.MAGIC = compiler_magic
+        self.loader._compiler_cls.MAGIC = compiler_magic
         
-        result = loader.apply_compiler_magic(data)
+        result = self.loader.apply_compiler_magic(self.data)
         self.assertEqual(result, magic_data)
+
+
+class TestLoaderApplyCompilerMagicTag(unittest.TestCase):
+    """
+    Tests for Loader.apply_compiler_magic
+    """
+    
+    path = '/abc/def/xyz.cpython.pyc'
+    
+    def setUp(self):
+        self.loader = default_loader()
+    
     
     def test_apply_compiler_magic_tag_no_magic_tag(self):
         """
         .apply_compiler_magic_tag should do nothing with no magic tag
         """
         
-        path = '/abc/def/xyz.cpython.pyc'
-        
-        loader = self.default_loader()
-        loader._compiler_cls.MAGIC_TAG = None
-        
-        result = loader.apply_compiler_magic_tag(path)
-        self.assertEqual(result, path)
+        result = self.loader.apply_compiler_magic_tag(self.path)
+        self.assertEqual(result, self.path)
     
     def test_apply_compiler_magic_tag_with_magic_tag(self):
         """
@@ -204,11 +220,9 @@ class TestLoader(unittest.TestCase):
         """
         
         magic_tag = 'custom-magic'
-        path = '/abc/def/xyz.cpython.pyc'
-        expected_path = '/abc/def/xyz.cpython.{}.pyc'.format(magic_tag)
+        magic_path = '/abc/def/xyz.cpython.{}.pyc'.format(magic_tag)
         
-        loader = self.default_loader()
-        loader._compiler_cls.MAGIC_TAG = magic_tag
+        self.loader._compiler_cls.MAGIC_TAG = magic_tag
         
-        result = loader.apply_compiler_magic_tag(path)
-        self.assertEqual(result, expected_path)
+        result = self.loader.apply_compiler_magic_tag(self.path)
+        self.assertEqual(result, magic_path)
