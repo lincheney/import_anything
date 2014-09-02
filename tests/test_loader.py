04f2b72b49da7f1a106914f628d67ae36695fa92
Cheney Lin <lincheney@gmail.com>
2014-09-02 14:20:08 +1000

    tests for Loader

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,5 +1,6 @@
 import unittest
 import unittest.mock as mock
+from unittest.mock import sentinel
 from import_anything import Loader, Compiler
 
 class TestLoader(unittest.TestCase):
@@ -8,19 +9,19 @@ class TestLoader(unittest.TestCase):
         .from_compiler should subclass and set COMPILER
         """
         
-        compiler = mock.Mock()
-        factory = Loader.from_compiler(compiler)
+        compiler_cls = mock.Mock(return_value = sentinel.compiler)
+        factory = Loader.from_compiler(compiler_cls)
         
         assert callable(factory)
         
         loader = factory('', 'path')
         self.assertIsInstance(loader, Loader)
-        self.assertIs(loader.compiler, compiler())
+        self.assertIs(loader.compiler, sentinel.compiler)
     
     @mock.patch.object(Loader, 'compiler')
     def test_get_data_compiled(self, compiler):
         """
-        .get_data should return the compiled data
+        .get_data should return the translated code
         when re-compiling
         """
         
@@ -48,28 +49,36 @@ class TestLoader(unittest.TestCase):
             self.assertEqual(int.from_bytes(result[8:12], 'little'), size)
             self.assertEqual(result[12:], data[12:])
     
-    @mock.patch.object(Loader, 'compiler')
     @mock.patch('builtins.compile')
-    def test_source_to_code_compiled(self, compile, compiler):
+    @mock.patch.object(Loader, 'compiler')
+    def test_source_to_code_compiled(self, compiler, compile):
         """
         .source_to_code should compile a code object
         when re-compiling
         """
         
-        compile.return_value = mock.sentinel.code
+        compile.return_value = sentinel.code
         
         loader = Loader('', 'path')
-        result = loader.source_to_code(None, mock.sentinel.path)
+        result = loader.source_to_code(None, sentinel.path)
         
-        compile.assert_called_once_with(compiler.make_ast_tree(), mock.sentinel.path, mock.ANY)
-        self.assertIs(result, mock.sentinel.code)
+        compile.assert_called_once_with(compiler.make_ast_tree(), sentinel.path, mock.ANY)
+        self.assertIs(result, sentinel.code)
     
-    def test_source_to_code_cached(self):
+    @mock.patch('importlib.machinery.SourceFileLoader.set_data', return_value = sentinel.super_result)
+    def test_set_data(self, super_set_data):
         """
-        .source_to_code should return pre-compiled code object
+        .set_data should replace data with recompiled code
         """
+        import marshal
         
-        loader = Loader('', 'path')
-        result = loader.source_to_code(None, None, original_code = mock.sentinel.code)
+        data = b'0' * 100
+        code = 'code'
         
-        self.assertIs(result, mock.sentinel.code)
+        with mock.patch.object(Loader, 'source_to_code', return_value = code):
+            loader = Loader('', 'path')
+            result = loader.set_data('bytecode path', data)
+            
+            super_set_data.assert_called_once_with('bytecode path', data[:12] + marshal.dumps('code'))
+            # returns the super call
+            self.assertIs(result, sentinel.super_result)
