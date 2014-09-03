379bb46338f1f208cccba0606059628483ffdf70
Cheney Lin <lincheney@gmail.com>
2014-09-03 20:22:40 +1000

    decorator to auto add pass statements to empty blocks

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,3 +34,25 @@ class TestUtils(unittest.TestCase):
         rest = 'abcdef'
         result = Utils.strip_indents(indent + rest)
         self.assertEqual(result, (len(indent), rest))
+    
+    def test_complete_blocks(self):
+        """
+        .complete_blocks should append 'pass' to all empty
+        code blocks (i.e. if,while,for,def,class,with etc.)
+        """
+        
+        indent = 2
+        body = 'abc'
+        @Utils.complete_blocks(indent_by = indent, body = body)
+        def source(block):
+            yield 1, 'line #1'
+            yield 2, block('block:')
+            yield 3, '  block body'
+            yield 4, block('empty block:')
+        
+        result = list(source())
+        self.assertEqual(len(result), 5)
+        
+        lineno, strings = zip(*result)
+        self.assertEqual(lineno, (1, 2, 3, 4, 4))
+        self.assertEqual(strings, ('line #1', 'block:', '  block body', 'empty block:', ' ' * indent + 'abc'))
