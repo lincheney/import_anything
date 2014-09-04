7d5da9fef7f52f3596dbed77aa7b9b70178a25c3
Cheney Lin <lincheney@gmail.com>
2014-09-04 21:28:50 +1000

    haml: inlined tag line parser

--- examples/haml/import_haml.py
+++ examples/haml/import_haml.py
@@ -22,8 +22,29 @@ class HamlCompiler(import_anything.Compiler):
                 if line.startswith('%'):
                     line = line[1:]
                 
-                for l in self.handle_tag(line.lstrip(), block):
-                    yield lineno, utils.indent(indent, l)
+                tag, string = re.match(r'([\w.#]*)(.*)', line.lstrip()).groups()
+                
+                classes = []
+                ids = []
+                tag, *rest = re.split(r'([#.])', tag)
+                tag = tag or 'div'
+                for prefix, name in zip(rest[::2], rest[1::2]):
+                    if prefix == '.':
+                        classes.append(name)
+                    elif prefix == '#':
+                        ids.append(name)
+                
+                attributes = None
+                if string.startswith('{'):
+                    attributes, string = utils.extract_structure(string)
+                
+                if string.startswith('='):
+                    string = string[1:]
+                else:
+                    string = repr(string)
+                
+                line = utils.indent(indent, 'with stack.add_tag({!r}, {}, {!r}, {!r}, {}):', tag, string, tuple(classes), tuple(ids), attributes)
+                yield lineno, block(line)
             
             else:
                 if line.startswith('='):
@@ -36,29 +57,6 @@ class HamlCompiler(import_anything.Compiler):
         
         yield lineno, utils.indent(2, 'return stack.render()')
         yield lineno, 'render = lambda **kwa: eval(_render.__code__, kwa)'
-    
-    def handle_tag(self, string, block):
-        tag, string = re.match(r'([\w.#]*)(.*)', string).groups()
-        
-        classes = []
-        ids = []
-        tag, *rest = re.split(r'([#.])', tag)
-        tag = tag or 'div'
-        for prefix, name in zip(rest[::2], rest[1::2]):
-            if prefix == '.':
-                classes.append(name)
-            elif prefix == '#':
-                ids.append(name)
-        
-        attributes = None
-        if string.startswith('{'):
-            attributes, string = utils.extract_structure(string)
-        
-        if string.startswith('='):
-            string = string[1:]
-        else:
-            string = repr(string)
-        yield block('with stack.add_tag({!r}, {}, {!r}, {!r}, {}):'.format(tag, string, tuple(classes), tuple(ids), attributes))
 
 loader = import_anything.Loader.factory(compiler = HamlCompiler, recompile = True)
 import_anything.Finder.register(loader, ['.haml'])
