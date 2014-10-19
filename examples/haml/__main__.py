3480581d783e178784120622fdc337bf01ebf023
Cheney Lin <lincheney@gmail.com>
2014-10-19 17:50:28 +1100

    update example to use variables

--- examples/haml/__main__.py
+++ examples/haml/__main__.py
@@ -3,14 +3,8 @@ if __name__ == '__main__':
     from . import main_haml
     
     haml = main_haml.render(
-        title = 'MyPage',
-        item = dict(type = 'massive', number = 4, urgency = 'urgent'),
-        sortcol = None,
-        sortdir = '/tmp',
-        href = '/index.html#more-stuff',
-        link = dict(href = '#'),
-        article = dict(number = 27, visibility = 'visible'),
-        link_to_remote = lambda *a, **kwa: None,
-        product = dict(id = 3),
+        a_string_passed_into_render = 'which will be rendered',
+        another_var = 'Which can be used for dynamic content',
+        list_variable = ['list', 'of', 'text'],
     )
     print(haml)
