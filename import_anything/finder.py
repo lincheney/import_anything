7e757016e1ad6429bafb1738ec107402c5a0aa22
Cheney Lin <lincheney@gmail.com>
2015-01-03 18:27:22 +1100

    make it work in python3.4

--- import_anything/finder.py
+++ import_anything/finder.py
@@ -1,36 +1,43 @@
 import importlib.machinery
+import importlib.util
 import sys
 
 class Finder(importlib.machinery.PathFinder):
     """
     Finder
-    
+
     Responsible for locating source files
     and loading them with the appropriate loader
     """
-    
+
     _loaders = []
     _suffixes = []
-    
+
     @classmethod
     def find_module(cls, fullname, path):
         details = list(zip(cls._loaders, cls._suffixes))
         if path is None:
             path = sys.path
-        
+
         for i in path:
             loader, portions = importlib.machinery.FileFinder(i, *details).find_loader(fullname)
             if loader:
                 return loader
-    
+
+    @classmethod
+    def find_spec(cls, fullname, path, target):
+        loader = cls.find_module(fullname, path)
+        if loader:
+            return importlib.util.spec_from_loader(fullname, loader)
+
     @classmethod
     def register(cls, loader, suffixes):
         """
         Register a loader to handle file names with @suffixes
-        
+
         @suffixes:      list of strings
         """
-        
+
         cls._loaders.append(loader)
         cls._suffixes.append(suffixes)
 
